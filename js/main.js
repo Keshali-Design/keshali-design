8acfbe41bdbc2c7d284b2329d376786ae9b6474a
Keshali-Design <keshalidesign@gmail.com>
2026-01-14 22:25:57 -0500

    Delete js/main.js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("year").textContent = new Date().getFullYear();
-
-// Menú móvil
-const nav = document.getElementById("nav");
-const toggle = document.getElementById("navToggle");
-
-toggle.addEventListener("click", () => nav.classList.toggle("open"));
